033835b83873defc7c882f9fbb94300fc94fd9e3
aventurella <aventurella@gmail.com>
2010-01-05 08:51:22 -0800

    began ConstellationDelegate

--- constellation/constellation/Constellation.php
+++ constellation/constellation/Constellation.php
@@ -1,7 +1,12 @@
 <?php
 require 'lib/galaxy/Galaxy.php';
+require 'ConstellationDelegate.php';
+
 class Constellation
 {
+	private $delegate;
+	private $galaxy;
+	
 	public static function galaxyForum()
 	{
 		$options  = array('id'     => 'com.galaxy.community',
@@ -17,6 +22,17 @@ class Constellation
                           'format' => Galaxy::kFormatJSON);*/
 		
 		return Galaxy::applicationWithOptions($options);
+	}
+	
+	public function __construct(ConstellationDelegate $delegate=null)
+	{
+		$options  = array('id'     => 'com.galaxy.community',
+		                  'key'    => '3be355bc06dc4923a3e894e5675e3f0a',
+		                  'secret' => '91da6fc8ae68944913ac4f2f549886b2',
+		                  'type'   => Galaxy::kApplicationForum,
+		                  'format' => Galaxy::kFormatJSON);
+		
+		$this->galaxy = Galaxy::applicationWithOptions($options);
 	}
 }
 ?>
