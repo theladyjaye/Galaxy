7ac3eae6dc395b3f5bf6d061c4d3ca0296bc4bc2
aventurella <aventurella@gmail.com>
2010-01-18 11:33:10 -0800

    api topic_delete() update for new topic/message logic

--- api/application/commands/GalaxyForum.php
+++ api/application/commands/GalaxyForum.php
@@ -123,10 +123,7 @@ class GalaxyForum extends GalaxyApplication
 		$channel  = GalaxyAPI::database(GalaxyAPIConstants::kDatabaseMongoDB, GalaxyAPI::databaseForId($context->channel), $options);
 		
 		// delete all messages
-		// finally delete the topic itself
-		
 		$channel->remove(array('topic' => $context->more));
-		$channel->remove(array('_id'   => $context->more));
 	}
 	
 	public function topics_get(GalaxyContext $context)
