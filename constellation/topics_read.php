468b2b981db411f5cd48493eed1cde06721c2ba2
aventurella <aventurella@gmail.com>
2010-01-18 19:55:02 -0800

    added per message editing when reading a topic

--- constellation/topics_read.php
+++ constellation/topics_read.php
@@ -7,6 +7,7 @@ $messages = $application->constellation->topic_messages($_GET['id']);
 $messages = json_decode($messages);
 
 $back     = implode('.', array_slice(explode('.', $_GET['id']), 0, 4));
+$channel  = substr($_GET['id'], 0, strrpos($_GET['id'], '.'));
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
@@ -28,7 +29,7 @@ $back     = implode('.', array_slice(explode('.', $_GET['id']), 0, 4));
 				$author_name = $message->author_name       ? $message->author_name : 'Unknown';
 			?>
 			<div style="padding-top:10px">
-				<div style="background-color:#efefef; padding:7px"><?php echo $message->title ?></div>
+				<div style="background-color:#efefef; padding:7px"><?php echo $message->title ?> | <a href="/moderator/message_edit.php?id=<?php echo $channel.'.'.$message->id ?>">Edit Message</a></div>
 				<div style="font-size:11px; font-style:italic">(Posted From: <a href="http://<?php echo $message->origin_domain ?>"><?php echo $message->origin_description ?></a> on <?php echo date('Y-m-d', strtotime($message->created))?> by: <?php echo $author_name ?>)</div>
 				<div style="padding-top:10px; padding-bottom:10px; border-bottom:1px solid #cccccc">
 						<div style="float:left;width:80px;"><img src="<?php echo $avatar ?>"></div>
